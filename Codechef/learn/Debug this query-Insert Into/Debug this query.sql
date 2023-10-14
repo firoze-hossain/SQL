76b3d595d17a0ee8039245d86b4d02ed0a071d90
Md. Firoze Hossain <firoze.hossain03@outlook.com>
2023-10-14 10:08:18 +0600

    Debug this query

---
+++ Codechef/learn/Debug this query-Insert Into/Debug this query.sql
@@ -0,0 +1,23 @@
+INSERT INTO
+    employee (Id, Name, Age, Address)
+VALUES (
+        1,
+        'John Smith',
+        25,
+        '123 Main St'
+    ), (
+        2,
+        'Sarah Johnson',
+        30,
+        '456 Broadway'
+    ), (
+        3,
+        'Michael Brown',
+        45,
+        '124 Hello Sot'
+    ), (
+        4,
+        'Jessica Davis',
+        28,
+        '321 Elm St'
+    );
